2b263049bd152504227c9ade7e7b6e17cbc95f11
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2012-05-21 10:55:46 +0200

    empathy-accounts: init cheese-gtk if needed
    The avatar chooser, which is not used by empathy-accounts, may use Cheese to
    take a photo.
    
    cheese_gtk_init() has been added during the 3.4 cycle so I bumped the dep to
    3.4.0 as that's the version I have tested it with it. It's an optionnal dep
    anyway so...
    
    https://bugzilla.gnome.org/show_bug.cgi?id=674553

--- src/empathy-accounts.c
+++ src/empathy-accounts.c
@@ -32,6 +32,10 @@
 #include <gtk/gtk.h>
 #include <glib/gi18n.h>
 
+#ifdef HAVE_CHEESE
+#include <cheese-gtk.h>
+#endif
+
 #include <telepathy-glib/account-manager.h>
 #include <telepathy-glib/defs.h>
 #include <telepathy-glib/util.h>
@@ -223,6 +227,13 @@ main (int argc, char *argv[])
   gint retval;
 
   g_thread_init (NULL);
+  g_type_init ();
+
+#ifdef HAVE_CHEESE
+  /* Used by the avatar chooser */
+  g_return_val_if_fail (cheese_gtk_init (&argc, &argv), 1);
+#endif
+
   empathy_init ();
 
   gtk_init (&argc, &argv);
