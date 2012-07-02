254c6ef0192ee7bc70530987a5e359cb4a04148c
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2012-07-02 15:09:00 +0200

    theme-adium: port to new coding styles
    Some part of this file was already using the new style so best to stay
    coherent.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=679255

--- libempathy-gtk/empathy-theme-adium.h
+++ libempathy-gtk/empathy-theme-adium.h
@@ -1,6 +1,5 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /*
- * Copyright (C) 2008-2009 Collabora Ltd.
+ * Copyright (C) 2008-2012 Collabora Ltd.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -28,49 +27,70 @@
 
 G_BEGIN_DECLS
 
-#define EMPATHY_TYPE_THEME_ADIUM         (empathy_theme_adium_get_type ())
-#define EMPATHY_THEME_ADIUM(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), EMPATHY_TYPE_THEME_ADIUM, EmpathyThemeAdium))
-#define EMPATHY_THEME_ADIUM_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), EMPATHY_TYPE_THEME_ADIUM, EmpathyThemeAdiumClass))
-#define EMPATHY_IS_THEME_ADIUM(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), EMPATHY_TYPE_THEME_ADIUM))
-#define EMPATHY_IS_THEME_ADIUM_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), EMPATHY_TYPE_THEME_ADIUM))
-#define EMPATHY_THEME_ADIUM_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), EMPATHY_TYPE_THEME_ADIUM, EmpathyThemeAdiumClass))
+/* TYPE MACROS */
+#define EMPATHY_TYPE_THEME_ADIUM \
+  (empathy_theme_adium_get_type ())
+#define EMPATHY_THEME_ADIUM(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), \
+    EMPATHY_TYPE_THEME_ADIUM, \
+    EmpathyThemeAdium))
+#define EMPATHY_THEME_ADIUM_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST((klass), \
+    EMPATHY_TYPE_THEME_ADIUM, \
+    EmpathyThemeAdiumClass))
+#define EMPATHY_IS_THEME_ADIUM(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj), \
+    EMPATHY_TYPE_THEME_ADIUM))
+#define EMPATHY_IS_THEME_ADIUM_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE((klass), \
+    EMPATHY_TYPE_THEME_ADIUM))
+#define EMPATHY_THEME_ADIUM_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), \
+    EMPATHY_TYPE_THEME_ADIUM, \
+    EmpathyThemeAdiumClass))
 
-typedef struct _EmpathyThemeAdium      EmpathyThemeAdium;
+
+
+typedef struct _EmpathyThemeAdium EmpathyThemeAdium;
 typedef struct _EmpathyThemeAdiumClass EmpathyThemeAdiumClass;
-typedef struct _EmpathyAdiumData       EmpathyAdiumData;
+typedef struct _EmpathyAdiumData EmpathyAdiumData;
 
-struct _EmpathyThemeAdium {
-	WebKitWebView parent;
-	gpointer priv;
+struct _EmpathyThemeAdium
+{
+  WebKitWebView parent;
+  gpointer priv;
 };
 
-struct _EmpathyThemeAdiumClass {
-	WebKitWebViewClass parent_class;
+struct _EmpathyThemeAdiumClass
+{
+  WebKitWebViewClass parent_class;
 };
 
-GType              empathy_theme_adium_get_type (void) G_GNUC_CONST;
-EmpathyThemeAdium *empathy_theme_adium_new      (EmpathyAdiumData *data,
-						 const gchar *variant);
-void               empathy_theme_adium_set_variant (EmpathyThemeAdium *theme,
-						    const gchar *variant);
-void               empathy_theme_adium_show_inspector (EmpathyThemeAdium *theme);
+GType empathy_theme_adium_get_type (void) G_GNUC_CONST;
 
-gboolean           empathy_adium_path_is_valid (const gchar *path);
+EmpathyThemeAdium *empathy_theme_adium_new (EmpathyAdiumData *data,
+    const gchar *variant);
+void empathy_theme_adium_set_variant (EmpathyThemeAdium *theme,
+                const gchar *variant);
+void empathy_theme_adium_show_inspector (EmpathyThemeAdium *theme);
 
-GHashTable        *empathy_adium_info_new (const gchar *path);
-const gchar *      empathy_adium_info_get_default_variant (GHashTable *info);
-GPtrArray *        empathy_adium_info_get_available_variants (GHashTable *info);
+/* not methods functions */
+
+gboolean empathy_adium_path_is_valid (const gchar *path);
+
+GHashTable *empathy_adium_info_new (const gchar *path);
+const gchar * empathy_adium_info_get_default_variant (GHashTable *info);
+GPtrArray * empathy_adium_info_get_available_variants (GHashTable *info);
 
 #define EMPATHY_TYPE_ADIUM_DATA (empathy_adium_data_get_type ())
-GType              empathy_adium_data_get_type (void) G_GNUC_CONST;
-EmpathyAdiumData  *empathy_adium_data_new (const gchar *path);
-EmpathyAdiumData  *empathy_adium_data_new_with_info (const gchar *path,
-						     GHashTable *info);
-EmpathyAdiumData  *empathy_adium_data_ref (EmpathyAdiumData *data);
-void               empathy_adium_data_unref (EmpathyAdiumData *data);
-GHashTable        *empathy_adium_data_get_info (EmpathyAdiumData *data);
-const gchar       *empathy_adium_data_get_path (EmpathyAdiumData *data);
-
+GType empathy_adium_data_get_type (void) G_GNUC_CONST;
+EmpathyAdiumData *empathy_adium_data_new (const gchar *path);
+EmpathyAdiumData *empathy_adium_data_new_with_info (const gchar *path,
+    GHashTable *info);
+EmpathyAdiumData *empathy_adium_data_ref (EmpathyAdiumData *data);
+void empathy_adium_data_unref (EmpathyAdiumData *data);
+GHashTable *empathy_adium_data_get_info (EmpathyAdiumData *data);
+const gchar *empathy_adium_data_get_path (EmpathyAdiumData *data);
 
 G_END_DECLS
 
