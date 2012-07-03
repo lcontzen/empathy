0cba1e794967d71172d0d2438fc5ea9f872a52f0
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2012-07-03 15:22:20 +0200

    add empathy-roster-model

---
+++ libempathy-gtk/empathy-roster-model.c
@@ -0,0 +1,29 @@
+/*
+ * empathy-roster-model.c
+ *
+ * Copyright (C) 2012 Collabora Ltd. <http://www.collabora.co.uk/>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#include "config.h"
+
+#include "empathy-roster-model.h"
+
+G_DEFINE_INTERFACE (EmpathyRosterModel, empathy_roster_model, G_TYPE_OBJECT)
+
+static void
+empathy_roster_model_default_init (EmpathyRosterModelInterface *iface)
+{
+}
---
+++ libempathy-gtk/empathy-roster-model.h
@@ -0,0 +1,57 @@
+/*
+ * empathy-roster-model.h
+ *
+ * Copyright (C) 2012 Collabora Ltd. <http://www.collabora.co.uk/>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifndef __EMPATHY_ROSTER_MODEL_H__
+#define __EMPATHY_ROSTER_MODEL_H__
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+typedef struct _EmpathyRosterModel EmpathyRosterModel;
+typedef struct _EmpathyRosterModelInterface EmpathyRosterModelInterface;
+
+struct _EmpathyRosterModelInterface
+{
+  /*<private>*/
+  GTypeInterface g_iface;
+
+  /* Virtual table */
+};
+
+GType empathy_roster_model_get_type (void);
+
+/* TYPE MACROS */
+#define EMPATHY_TYPE_ROSTER_MODEL \
+  (empathy_roster_model_get_type ())
+#define EMPATHY_ROSTER_MODEL(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), \
+    EMPATHY_TYPE_ROSTER_MODEL, \
+    EmpathyRosterModel))
+#define EMPATHY_IS_ROSTER_MODEL(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj), \
+    EMPATHY_TYPE_ROSTER_MODEL))
+#define EMPATHY_ROSTER_MODEL_GET_IFACE(obj) \
+  (G_TYPE_INSTANCE_GET_INTERFACE ((obj), \
+    EMPATHY_TYPE_ROSTER_MODEL, \
+    EmpathyRosterModelInterface))
+
+G_END_DECLS
+
+#endif /* #ifndef __EMPATHY_ROSTER_MODEL_H__*/
